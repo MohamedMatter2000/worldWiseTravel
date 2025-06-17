3d9cdf9b677500b9b501a55091a1a20eda64183c
moahamedmatter <mahmouedmatter@yahoo.com>
2025-06-17 18:37:34 +0300

    initnetlify

--- src/components/CityItem.jsx
+++ src/components/CityItem.jsx
@@ -22,7 +22,7 @@ export default function CityItem({ city }) {
         className={`${styles.cityItem} ${
           id === currentCity.id ? styles["cityItem--active"] : ""
         }`}
-        to={`${id}?lat=${position.lat}&lng=${position.lng}`}
+        to={`${id}?lat=${position?.lat}&lng=${position?.lng}`}
       >
         <span className={styles.emoji}>{emoji}</span>
         <h3 className={styles.name}>{cityName}</h3>
